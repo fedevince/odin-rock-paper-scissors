d8ff0f903b385bb89a2d4a88b4f7cac5bf5f1829
Federico Vincelli <vincellifederico@gmail.com>
2025-08-17 19:57:45 +0200

    Implement draw in playRound function

--- script.js
+++ script.js
@@ -15,9 +15,29 @@ function getComputerChoice() {
 // ask user for choice
 // loop to validate choice
 function getHumanChoice(){
-    let humanChoice = prompt("Pick rock, paper or scissors.");
+    let humanChoice = prompt("Pick rock, paper or scissors.").toLowerCase();
     while (!(humanChoice === "rock" || humanChoice === "paper" || humanChoice === "scissors")) {
         humanChoice = prompt("Please enter a valid option ");
     }
     return humanChoice;
 }
+
+let computerScore = 0;
+let humanScore = 0;
+
+// play round:
+// get computer and human choice and save them as string variables
+// ensure human choice is lower case
+// function play round with the two choices as parameters
+// round logic: if equal draw, three if options for each human choice
+// increment score for winner of the round
+const computerSelection = getComputerChoice();
+const humanSelection = getHumanChoice();
+
+function playRound(computerSelection, humanSelection) {
+    if (computerSelection === humanSelection) {
+        return "Draw";
+    }
+}
+
+
