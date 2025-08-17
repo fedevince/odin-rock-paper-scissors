235dd1a26eafb7043fea8e8dc0089763082027ca
Federico Vincelli <vincellifederico@gmail.com>
2025-08-17 19:31:35 +0200

    Add getComputerChoice function

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-console.log("hello!")
-
-console.log(42)
+// get computer choice: provides a random choice between rock paper and scissors
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * 3);
+    if (choice === 0){
+        return "rock";
+    } else if (choice === 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
